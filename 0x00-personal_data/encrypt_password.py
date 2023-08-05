975d61046267e3ea2c66d1101fe7de64699e3ce6
TessyJames28 <tessyjames28@gmail.com>
2023-08-05 19:38:53 +0100

    Check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,3 +8,10 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashpw = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashpw
+
+
+def is_valid(hash_password: bytes, password: str) -> bool:
+    """check valid password"""
+    if bcrypt.checkpw(password.encode('utf-8'), hash_password):
+        return True
+    return False
