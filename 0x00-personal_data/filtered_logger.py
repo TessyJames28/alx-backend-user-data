753de20044e5a69d3c63c71426d5397d7b32ad41
TessyJames28 <tessyjames28@gmail.com>
2023-08-05 16:36:02 +0100

    Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """Regex-ing"""
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator) -> str:
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:  # nopep8
     """returns the log message obfuscated"""
     for val in fields:
-        message = re.sub(r"{}=.*?;".format(val), "{}={};".format(val, redaction), message)  # nopep8
+        pattern = fr"{val}=.*?(?={separator})"
+        message = re.sub(pattern, f"{val}={redaction}", message)
     return message
