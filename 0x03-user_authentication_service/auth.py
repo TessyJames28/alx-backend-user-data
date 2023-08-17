41ebad8782a69cc77cc88d1620f5b9e83e629b1d
TessyJames28 <tessyjames28@gmail.com>
2023-08-17 15:43:00 +0100

    Destroy session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -56,23 +56,26 @@ class Auth:
             user = self._db.find_user_by(email=email)
             sess_id = _generate_uuid()
             user_update = self._db.update_user(user.id, session_id=sess_id)
-            return sess_id
+            return user.session_id
         except NoResultFound:
             return None
 
-    def get_user_from_session_id(self, session_id: str) -> Union[User | None]:
+    def get_user_from_session_id(self, session_id: str) -> User:
         """Find user by session_id"""
-        user = self._db.find_user_by(session_id=session_id)
-        if not user:
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
             return None
-        return user
 
     def destroy_session(self, user_id: int) -> None:
         """Destroy session method"""
-        user = self._db.find_user_by(id=user_id)
-        if user:
+        try:
+            user = self._db.find_user_by(id=user_id)
             self._db.update_user(user_id, session_id=None)
             return user.session_id
+        except NoResultFound:
+            return None
 
     def get_reset_password_token(self, email: str) -> str:
         """Generate reset password token method"""
