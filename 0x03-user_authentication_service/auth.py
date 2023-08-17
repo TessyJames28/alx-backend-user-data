b1fa7b31135b82ce62b807dff5f6fa2819e3aa2a
TessyJames28 <tessyjames28@gmail.com>
2023-08-17 05:22:10 +0100

    create a register user method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """Hash password"""
 import bcrypt
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -8,3 +10,21 @@ def _hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hash_passwd = bcrypt.hashpw(password.encode("utf-8"), salt)
     return hash_passwd
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """return a User object"""
+        user = User(email=email, hashed_password=password)
+        if user.email:
+            raise ValueError("User {} already exists".format(user.email))
+        else:
+            hash = _hash_password(user.password)
+            self._db._session.add(user)
+        return user
--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -58,3 +58,32 @@ try:
     print("Password updated")
 except ValueError:
     print("Error")
+
+
+"""task 4"""
+"""
+Main file
+"""
+from auth import _hash_password
+
+print(_hash_password("Hello Holberton"))
+
+"""task 5"""
+from auth import Auth
+
+email = 'me@me.com'
+password = 'mySecuredPwd'
+
+auth = Auth()
+
+try:
+    user = auth.register_user(email, password)
+    print("successfully created a new user!")
+except ValueError as err:
+    print("could not create a new user: {}".format(err))
+
+try:
+    user = auth.register_user(email, password)
+    print("successfully created a new user!")
+except ValueError as err:
+    print("could not create a new user: {}".format(err))
