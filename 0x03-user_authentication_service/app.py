4e705db7685240d662b67a3076c2a5fa64e65fb8
TessyJames28 <tessyjames28@gmail.com>
2023-08-17 06:24:58 +0100

    created a user function

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,15 +1,34 @@
 #!/usr/bin/env python3
 """Basic Flask app"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+from sqlalchemy.orm.exc import NoResultFound
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/")
 def index():
     """returns a json response"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", strict_slashes=False, methods=["POST"])
+def users():
+    """user function"""
+    try:
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user_exist = AUTH._db.find_user_by(email=email)
+    except NoResultFound:
+        user = AUTH.register_user(email, password)
+        value = {
+            "email": "{}".format(user.email),
+            "message": "user created"}
+        return jsonify(value), 200
+    return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
